e7d5a2cd72c73c8581cdb971a4a0c1932165216b
LuisCT13 <luisangelcovtam@gmail.com>
2024-12-04 20:46:31 -0700

    Cabeza y nariz pta la wea q pesao

--- openglwidget.cpp
+++ openglwidget.cpp
@@ -6,42 +6,490 @@ OpenGLWidget::OpenGLWidget() {
     focoX = 0.0f;
     focoY = 1.0f;
     setMouseTracking(true);
+    //QColor rosaCuerpo(255, 188, 191);
     objeto3D=new Objeto3D();
     Superficie* s=new Superficie(1,0,0);
-    s->agregar(new Vertice(0,0,0));
-    s->agregar(new Vertice(0.5,0,0));
-    s->agregar(new Vertice(0.5,0.5,0));
-    s->agregar(new Vertice(0,0.5,0));
+    float x1 = 510, x2 = 460, y1 = 420, y2 = 321, z1 = 390, z2 = 340, z5 = 1, z6 = 0.9;
+    float x3 = 0.4, x4 = 0.6, y3= 0.6, y4 = 0.8;
+
+
+
+    // ---- NARIZ DEL CERDO ----
+
+    //frente
+    s->agregar(new Vertice(x4,y4,z5));
+    s->agregar(new Vertice(x3,y4,z5));
+    s->agregar(new Vertice(x3,y3,z5));
+    s->agregar(new Vertice(x4,y3,z5));
     objeto3D->agregar(s);
+    // objeto3D->agregarCabeza(s);
+
+
+
+    //cara derecha
     s = new Superficie(1,0,0);
-    s->agregar(new Vertice(0.5, 0, 0));
-    s->agregar(new Vertice(0.5, 0, -0.5));
-    s->agregar(new Vertice(0.5, 0.5, -0.5));
-    s->agregar(new Vertice(0.5, 0.5, 0));
+    s->agregar(new Vertice(x3, y4, z5));
+    s->agregar(new Vertice(x3, y4, z6));
+    s->agregar(new Vertice(x3, y3, z6));
+    s->agregar(new Vertice(x3, y3, z5));
     objeto3D->agregar(s);
+    // objeto3D->agregarCabeza(s);
+
+
+    //cara trasera
     s = new Superficie(1,0,0);
-    s->agregar(new Vertice(0.5, 0, -0.5));
-    s->agregar(new Vertice(0, 0, -0.5));
-    s->agregar(new Vertice(0, 0.5, -0.5));
-    s->agregar(new Vertice(0.5, 0.5, -0.5));
+    s->agregar(new Vertice(x4,y4,z6));
+    s->agregar(new Vertice(x3,y4,z6));
+    s->agregar(new Vertice(x3,y3,z6));
+    s->agregar(new Vertice(x4,y3,z6));
     objeto3D->agregar(s);
+    // objeto3D->agregarCabeza(s);
+
+
+    //cara izquierda
     s = new Superficie(1,0,0);
-    s->agregar(new Vertice(0, 0, -0.5));
-    s->agregar(new Vertice(0, 0, 0));
-    s->agregar(new Vertice(0, 0.5, 0));
-    s->agregar(new Vertice(0, 0.5, -0.5));
+    s->agregar(new Vertice(x4, y4, z5));
+    s->agregar(new Vertice(x4, y4, z6));
+    s->agregar(new Vertice(x4, y3, z6));
+    s->agregar(new Vertice(x4, y3, z5));
     objeto3D->agregar(s);
+    // objeto3D->agregarCabeza(s);
+
+
+    //cara superior
     s = new Superficie(1,0,0);
-    s->agregar(new Vertice(0, 0.5, 0));
-    s->agregar(new Vertice(0.5, 0.5, 0));
-    s->agregar(new Vertice(0.5, 0.5, -0.5));
-    s->agregar(new Vertice(0, 0.5, -0.5));
+    s->agregar(new Vertice(x4, y3, z6));
+    s->agregar(new Vertice(x3, y3, z6));
+    s->agregar(new Vertice(x3, y3, z5));
+    s->agregar(new Vertice(x4, y3, z5));
     objeto3D->agregar(s);
+    // objeto3D->agregarCabeza(s);
+
+
+    //cara inferior
     s = new Superficie(1,0,0);
-    s->agregar(new Vertice(0, 0, 0));
-    s->agregar(new Vertice(0, 0, -0.5));
-    s->agregar(new Vertice(0.5, 0, -0.5));
-    s->agregar(new Vertice(0.5, 0, 0));
+    s->agregar(new Vertice(x4, y4, z6));
+    s->agregar(new Vertice(x4, y4, z5));
+    s->agregar(new Vertice(x3, y4, z5));
+    s->agregar(new Vertice(x3, y4, z6));
+    objeto3D->agregar(s);
+    // objeto3D->agregarCabeza(s);
+
+
+
+
+
+
+
+    // ---- CABEZA DEL CERDO ----
+    //y menor 0.5 y y mayor de 1
+    // la x menor va a ser de 0.2 a 0.7
+    //cara principal
+    s = new Superficie(1,0,0);
+    s->agregar(new Vertice( 0.2,0.5,0.9));
+    s->agregar(new Vertice( 0.7,0.5,0.9));
+    s->agregar(new Vertice( 0.7,1,0.9));
+    s->agregar(new Vertice( 0.2,1,0.9));
+    objeto3D->agregar(s);
+    // objeto3D->agregarCabeza(s);
+
+
+    //cara derecha
+    s = new Superficie(1,0,0);
+    s->agregar(new Vertice( 0.7, 0.5, 0.9));
+    s->agregar(new Vertice( 0.7, 0.5, 0.5));
+    s->agregar(new Vertice( 0.7, 1,  0.5));
+    s->agregar(new Vertice( 0.7, 1, 0.9));
+    objeto3D->agregar(s);
+    // objeto3D->agregarCabeza(s);
+
+
+    //cara trasera
+    s = new Superficie(1,0,0);
+    s->agregar(new Vertice( 0.7, 0.5,  0.5));
+    s->agregar(new Vertice( 0.2, 0.5,  0.5));
+    s->agregar(new Vertice( 0.2, 1, 0.5));
+    s->agregar(new Vertice( 0.7, 1, 0.5));
+    objeto3D->agregar(s);
+    // objeto3D->agregarCabeza(s);
+
+
+    //cara izquierda
+    s = new Superficie(1,0,0);
+    s->agregar(new Vertice( 0.2, 0.5,  0.5));
+    s->agregar(new Vertice( 0.2, 0.5, 0.9));
+    s->agregar(new Vertice( 0.2, 1, 0.9));
+    s->agregar(new Vertice( 0.2, 1,  0.5));
+    objeto3D->agregar(s);
+    // objeto3D->agregarCabeza(s);
+
+
+    //cara superior
+    s = new Superficie(1,0,0);
+    s->agregar(new Vertice( 0.2, 1, 0.9));
+    s->agregar(new Vertice( 0.7, 1, 0.9));
+    s->agregar(new Vertice( 0.7, 1, 0.5));
+    s->agregar(new Vertice( 0.2, 1, 0.5));
+    objeto3D->agregar(s);
+    // objeto3D->agregarCabeza(s);
+
+
+    //cara inferior
+    s = new Superficie(1,0,0);
+    s->agregar(new Vertice( 0.2, 0.5, 0.9));
+    s->agregar(new Vertice( 0.2, 0.5,  0.5));
+    s->agregar(new Vertice( 0.7, 0.5,  0.5));
+    s->agregar(new Vertice( 0.7, 0.5, 0.9));
+    objeto3D->agregar(s);
+    // objeto3D->agregarCabeza(s);
+
+
+
+
+    // ---- PATA SUPERIOR IZQUIERDA ----
+
+    int z3 = 240/100, z4 = 190/100;
+
+    //parte de ARRIBA
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(390, y2, z4));
+    s->agregar(new Vertice(440, y2, z4));
+    s->agregar(new Vertice(440, y2, z3));
+    s->agregar(new Vertice(390, y2, z3));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataSupIzq(s);
+    // objeto3D->agregarPatas(s);
+
+
+    //parte de abajo
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(390, y1, z4));
+    s->agregar(new Vertice(440, y1, z4));
+    s->agregar(new Vertice(440, y1, z3));
+    s->agregar(new Vertice(390, y1, z3));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataSupIzq(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de derecha
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(440, y1, z4));
+    s->agregar(new Vertice(440, y1, z3));
+    s->agregar(new Vertice(440, y2, z3));
+    s->agregar(new Vertice(440, y2, z4));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataSupIzq(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de izquierda
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(390, y1, z4));
+    s->agregar(new Vertice(390, y1, z3));
+    s->agregar(new Vertice(390, y2, z3));
+    s->agregar(new Vertice(390, y2, z4));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataSupIzq(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de atrás
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(390, y1, z3));
+    s->agregar(new Vertice(440, y1, z3));
+    s->agregar(new Vertice(440, y2, z3));
+    s->agregar(new Vertice(390, y2, z3));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataSupIzq(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de enfrente
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(390, y1, z4));
+    s->agregar(new Vertice(440, y1, z4));
+    s->agregar(new Vertice(440, y2, z4));
+    s->agregar(new Vertice(390, y2, z4));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataSupIzq(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    // ---- PATA INFERIOR IZQUIERDA ----
+    //parte de ARRIBA
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(390, y2, 340));
+    s->agregar(new Vertice(440, y2, 340));
+    s->agregar(new Vertice(440, y2, 390));
+    s->agregar(new Vertice(390, y2, 390));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataInfIzq(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de abajo
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(390, y1, 340));
+    s->agregar(new Vertice(440, y1, 340));
+    s->agregar(new Vertice(440, y1, 390));
+    s->agregar(new Vertice(390, y1, 390));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataInfIzq(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de derecha
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(440, y1, 340));
+    s->agregar(new Vertice(440, y1, 390));
+    s->agregar(new Vertice(440, y2, 390));
+    s->agregar(new Vertice(440, y2, 340));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataInfIzq(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de izquierda
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(390, y1, 340));
+    s->agregar(new Vertice(390, y1, 390));
+    s->agregar(new Vertice(390, y2, 390));
+    s->agregar(new Vertice(390, y2, 340));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataInfIzq(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de atrás
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(390, y1, 390));
+    s->agregar(new Vertice(440, y1, 390));
+    s->agregar(new Vertice(440, y2, 390));
+    s->agregar(new Vertice(390, y2, 390));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataInfIzq(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de enfrente
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(390, y1, 340));
+    s->agregar(new Vertice(440, y1, 340));
+    s->agregar(new Vertice(440, y2, 340));
+    s->agregar(new Vertice(390, y2, 340));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataInfIzq(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    // ---- PATA INFERIOR DERECHA ----
+
+
+    //parte de ARRIBA
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(x2, y2, z2));
+    s->agregar(new Vertice(x1, y2, z2));
+    s->agregar(new Vertice(x1, y2, z1));
+    s->agregar(new Vertice(x2, y2, z1));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataInfDer(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de abajo
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(x2, y1, z2));
+    s->agregar(new Vertice(x1, y1, z2));
+    s->agregar(new Vertice(x1, y1, z1));
+    s->agregar(new Vertice(x2, y1, z1));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataInfDer(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de derecha
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(x1, y1, z2));
+    s->agregar(new Vertice(x1, y1, z1));
+    s->agregar(new Vertice(x1, y2, z1));
+    s->agregar(new Vertice(x1, y2, z2));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataInfDer(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de izquierda
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(x2, y1, z2));
+    s->agregar(new Vertice(x2, y1, z1));
+    s->agregar(new Vertice(x2, y2, z1));
+    s->agregar(new Vertice(x2, y2, z2));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataInfDer(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de atrás
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(x2, y1, z1));
+    s->agregar(new Vertice(x1, y1, z1));
+    s->agregar(new Vertice(x1, y2, z1));
+    s->agregar(new Vertice(x2, y2, z1));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataInfDer(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de enfrente
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(x2, y1, z2));
+    s->agregar(new Vertice(x1, y1, z2));
+    s->agregar(new Vertice(x1, y2, z2));
+    s->agregar(new Vertice(x2, y2, z2));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataInfDer(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    // ---- PATA SUPERIOR DERECHA ----
+    //parte de ARRIBA
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(x2, y2, z4));
+    s->agregar(new Vertice(x1, y2, z4));
+    s->agregar(new Vertice(x1, y2, z3));
+    s->agregar(new Vertice(x2, y2, z3));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataSupDer(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de abajo
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(x2, y1, z4));
+    s->agregar(new Vertice(x1, y1, z4));
+    s->agregar(new Vertice(x1, y1, z3));
+    s->agregar(new Vertice(x2, y1, z3));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataSupDer(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de derecha
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(x1, y1, z4));
+    s->agregar(new Vertice(x1, y1, z3));
+    s->agregar(new Vertice(x1, y2, z3));
+    s->agregar(new Vertice(x1, y2, z4));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataSupDer(s);
+    // objeto3D->agregarPatas(s);
+
+
+
+    //parte de izquierda
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(x2, y1, z4));
+    s->agregar(new Vertice(x2, y1, z3));
+    s->agregar(new Vertice(x2, y2, z3));
+    s->agregar(new Vertice(x2, y2, z4));
+    objeto3D->agregar(s);
+    //objeto3D->agregarPataSupDer(s);
+    //objeto3D->agregarPatas(s);
+
+
+
+    //parte de atrás
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(x2, y1, z3));
+    s->agregar(new Vertice(x1, y1, z3));
+    s->agregar(new Vertice(x1, y2, z3));
+    s->agregar(new Vertice(x2, y2, z3));
+    objeto3D->agregar(s);
+    //objeto3D->agregarPataSupDer(s);
+    //objeto3D->agregarPatas(s);
+
+
+
+    //parte de enfrente
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(x2, y1, z4));
+    s->agregar(new Vertice(x1, y1, z4));
+    s->agregar(new Vertice(x1, y2, z4));
+    s->agregar(new Vertice(x2, y2, z4));
+    objeto3D->agregar(s);
+    // objeto3D->agregarPataSupDer(s);
+    //objeto3D->agregarPatas(s);
+
+
+
+
+    // ---- CUERPO DEL CERDO ----
+    //lado derecho
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(520, 220, 180));
+    s->agregar(new Vertice(520, 220, 380));
+    s->agregar(new Vertice(520, 320, 380));
+    s->agregar(new Vertice(520, 320, 180));
+    objeto3D->agregar(s);
+
+    //parte de enfrente
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(380, 220, 180));
+    s->agregar(new Vertice(520, 220, 180));
+    s->agregar(new Vertice(520, 320, 180));
+    s->agregar(new Vertice(380, 320, 180));
+    objeto3D->agregar(s);
+
+    //parte de atrás
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(380, 220, 380));
+    s->agregar(new Vertice(520, 220, 380));
+    s->agregar(new Vertice(520, 320, 380));
+    s->agregar(new Vertice(380, 320, 380));
+    objeto3D->agregar(s);
+
+    //lado izquierdo
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(380, 220, 180));
+    s->agregar(new Vertice(380, 220, 380));
+    s->agregar(new Vertice(380, 320, 380));
+    s->agregar(new Vertice(380, 320, 180));
+    objeto3D->agregar(s);
+
+    //parte de arriba
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(380, 220, 180));
+    s->agregar(new Vertice(520, 220, 180));
+    s->agregar(new Vertice(520, 220, 380));
+    s->agregar(new Vertice(380, 220, 380));
+    objeto3D->agregar(s);
+
+    //parte de abajo
+    s = new Superficie(1,0,0);// X    Y    Z
+    s->agregar(new Vertice(380, 320, 180));
+    s->agregar(new Vertice(520, 320, 180));
+    s->agregar(new Vertice(520, 320, 380));
+    s->agregar(new Vertice(380, 320, 380));
     objeto3D->agregar(s);
 }
 
