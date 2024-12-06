c6ed0e5fb50435a0a08571b18fe14859f27ce3ec
LuisCT13 <luisangelcovtam@gmail.com>
2024-12-05 20:49:27 -0700

    alv ahora si funcionan las copias xd

--- cerdo.cpp
+++ cerdo.cpp
@@ -65,14 +65,15 @@ void Cerdo::trasladar(float x, float y, float z){
     pataSupDer->trasladar(x,y,z);
     pataSupIzq->trasladar(x,y,z);
 }
-Cerdo* Cerdo::copia(float x, float y, float z){
+Cerdo* Cerdo::copia(){
     // Crear las copias de todas las partes del cerdo
     Cerdo *copia= new Cerdo();
-    copia->cabezaCerdo=this->cabezaCerdo->copia();
-    copia->torsoCerdo=this->torsoCerdo->copia();
-    copia->pataInfDer=this->pataInfDer->copia();
-    copia->pataInfIzq=this->pataInfIzq->copia();
-    copia->pataSupDer=this->pataSupDer->copia();
-    copia->pataSupIzq=this->pataSupIzq->copia();
+    copia->cabezaCerdo=this->cabezaCerdo;
+    copia->torsoCerdo=this->torsoCerdo;
+    copia->pataInfDer=this->pataInfDer;
+    copia->pataInfIzq=this->pataInfIzq;
+    copia->pataSupDer=this->pataSupDer;
+    copia->pataSupIzq=this->pataSupIzq;
     return copia;
 }
+
